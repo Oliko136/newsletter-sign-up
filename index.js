ee9ae7b9860568c424e8324cd31b507037fa1821
Oliko136 <olchick136@gmail.com>
2025-03-04 16:41:18 +0400

    Add success message

--- index.js
+++ index.js
@@ -16,9 +16,21 @@ function handleSubmit(e) {
 function validateEmail(email) {
     const emailRegex = /^\S+@\S+$/g;
     
-    emailRegex.test(email) ? renderError(' ') : renderError('Valid email required');
+    emailRegex.test(email) ? renderSuccess(email) : renderError('Valid email required');
 }
 
 function renderError(message) {
     errorEl.textContent = message;
+}
+
+function renderSuccess(email) {
+    document.body.innerHTML = `
+    <div>
+        <h2>Thanks for subscribing!</h2>
+        <p>
+            A confirmation email has been sent to ${email}. 
+            Please open it and click the button inside to confirm your subscription.
+        </p>
+        <button type="button">Dismiss message</button>
+    </div>`
 }
